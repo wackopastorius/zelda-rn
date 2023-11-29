accb70a0d5a890958e6d50e6bbc75936c00ec653
wackopastorius <ladaegolaeva@gmail.com>
2023-11-29 08:55:58 +0100

    changed veg recipes

--- src/screens/VegRecipes.js
+++ src/screens/VegRecipes.js
@@ -1,68 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, ActivityIndicator, FlatList, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import cookbook from "../api/cookbook";
-import axios from "axios";
 
-const VegRecipes = ({}) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+const VegRecipes = () => {
+    console.log("Hi there");
+    const [results, setResults] = useState([]);
+
+    const searchApi = async () => {
+        try {
+            const response = await cookbook.get("/vegrecipes/");
+            setResults(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get("/vegrecipes");
-                setData(response.data);
-                setLoading(false);
-            } catch (err) {
-                setError(err);
-                setLoading(false);
-            }
-        };
-
-        fetchData();
+        searchApi();
     }, []);
 
-    if (loading) {
-        return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <ActivityIndicator/>
-            </View>
-        );
-    }
-
-    if (error) {
-        return (
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>An error occurred</Text>
-          </View>
-        );
-    }
-    
-    const renderItem = ({item}) => {
-        return (
-          <View
-            style={{
-              padding: 10,
-              marginVertical: 8,
-              marginHorizontal: 16,
-              backgroundColor: '#f9c2ff',
-            }}>
-            <Text style={{fontSize: 18}}>{item.title}</Text>
-            <Text style={{fontSize: 14}}>{item.body}</Text>
-          </View>
-        );
-      };
-
     return (
-        <SafeAreaView
-            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <FlatList
-                data={data}
-                renderItem={renderItem}
-                keyExtractor={item => item.id}
-            />
-        </SafeAreaView>
+        <View>
+            <Text>hey</Text>
+        </View>
     );
 };
 
