f40c8436cff1272f2f3e6b9a83a07004cafe0543
mickepetersson <per.mikael.petersson@gmail.com>
2023-11-30 10:39:49 +0100

    updated to local ip

--- src/screens/SeafoodRecipes.js
+++ src/screens/SeafoodRecipes.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import cookbook from "../api/cookbook";
+import useResults from "../hooks/useResults";
 
-const SeafoodRecipes = ({}) => {
-    return <View>
-        <Text>Seafood Recipes</Text>
-    </View>
+const SeafoodRecipes = () => {
+    const [results, errorMessage] = useResults();
+
+    
+    return (
+        <View>
+            <Text>Seafood Recipes</Text>
+            <FlatList
+                data={results}
+                keyExtractor={(result) => result.id}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text>{item.id}</Text>
+                        <Text>{item.name}</Text>
+                    </View>
+                )}
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
