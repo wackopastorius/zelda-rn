64c07f5372ca381999521d8ef09a28b4985e0642
MichaelGranbäck <michael.granback@outlook.com>
2023-12-02 13:08:16 +0100

    Added FlatList to be able to view posts

--- src/screens/MeatPoultryRecipes.js
+++ src/screens/MeatPoultryRecipes.js
@@ -1,14 +1,27 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import cookbook from '../api/cookbook';
+import useResults from '../hooks/useResults';
 
-const MeatPoultryRecipes = ({}) => {
-    return <View>
-        <Text>Meat & Poultry Recipes</Text>
+const MeatPoultryRecipes = () => {
+  const [results, errorMessage] = useResults('meatrecipes');
+  return (
+    <View>
+      <Text>Meat & Poultry Recipes</Text>
+      <FlatList
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.id}</Text>
+            <Text>{item.name}</Text>
+          </View>
+        )}
+      />
     </View>
+  );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default MeatPoultryRecipes;
